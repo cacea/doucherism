2d82381d6f015b4fc61395e23c83d87101751b35
Angel Rodriguez <zerosoul13@gmail.com>
2013-08-03 05:07:06 +0000

    Removed unneeded lines of code as they are now used on the template custom filter

--- blog_post/views.py
+++ blog_post/views.py
@@ -9,11 +9,6 @@ from time import gmtime, strftime
 def home(request):
     blog_template = "blogs.html"
     list_all_posts = BlogEntry.objects.all().values() # Con esto se listan todos los elementos de que se encuentren en la DB
-    temp_counter_for_image = 0
-    for img_path in list_all_posts:
-    	temp_image_path = list_all_posts[temp_counter_for_image]['blog_post_image']
-    	image_file = temp_image_path.split('/')[-1]
-    	temp_counter_for_image +=1
     return render(request, blog_template, locals())
 
 def register_user_post(request):
