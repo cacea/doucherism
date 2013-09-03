8f219c3fd416b558617825042d58728435faa4c0
Angel Rodriguez <zerosoul13@gmail.com>
2013-09-03 05:04:23 +0000

    Added login/logut option and templates. Views were added and now url has a parameter filter to reach the blog categories.

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from blog_post.models import BlogEntry, Category
 from blog_post.forms import BlogPostForm
-from django.shortcuts import HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect, redirect
 from time import gmtime, strftime
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 """
 This method will display all user posts (newest first)
 """
-def home(request):
+def blogs(request, category):
     blog_template = "blogs.html"
-    list_all_posts = BlogEntry.objects.all().order_by('-blog_pub_date').values('blog_post_name', 'blog_post_content', 'blog_post_category__name', 'blog_post_image') # List all posts on DB ordered by date (newest first).
+    list_all_posts = BlogEntry.objects.filter(blog_post_category__name = category).order_by('-blog_pub_date').values('blog_post_name', 'blog_post_content', 'blog_post_category__name', 'blog_post_image') # List all posts on DB ordered by date (newest first).
     return render(request, blog_template, locals())
 
 """
@@ -28,3 +30,19 @@ def register_user_post(request):
     else:
         form = BlogPostForm()
     return render(request, register_user_template, locals())
+
+def login_user(request):
+    template = "login.html"
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                pass
+    else:
+        return render(request, template, locals())
+
--- doucherism/urls.py
+++ doucherism/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 from django.conf import settings
 import blog_post.views
 
@@ -8,13 +9,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', blog_post.views.home),
+    url(r'^$', lambda x: HttpResponseRedirect('/blog/douchness')),
+    url(r'blog/(?P<category>\w+)/$', blog_post.views.blogs),
     url(r'^register/', blog_post.views.register_user_post),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^login/$', blog_post.views.login_user),
     url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media_files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-            #(r'^media_files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT#, 'show_indexes':True#}),
 )
